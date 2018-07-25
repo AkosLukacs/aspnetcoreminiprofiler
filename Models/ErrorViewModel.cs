64703a1a7ab80a84f3ed31a80d69474f100aa9d3
Lukács Ákos <AkosLukacs42@gmail.com>
2018-07-25 16:06:50 +0200

    start

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace authsample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
